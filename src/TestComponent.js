cde8bf8579a086bd952b3927c02241eef8c43375
Ben Sarmiento <me@bensarmiento.com>
2015-10-08 00:37:49 +0800

    Use state

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -1,19 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Greeter extends Component {
   render() {
     return (
       <h1>Hello, {this.props.name}!</h1>
-    );
+    )
+  }
+}
+
+class ChatList extends Component {
+  render() {
+    var chats = this.props.data.map(
+      chat => <li>{chat}</li>
+    )
+    return (
+      <ul>{chats}</ul>
+    )
   }
 }
 
 export class TestComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { data: [] }
+  }
   render() {
     return (
       <div>
-        <Greeter name="Ben222" />
+        <ChatList data={this.state.data} />
+        <Greeter name="Ben" />
       </div>
-    );
+    )
   }
 }
